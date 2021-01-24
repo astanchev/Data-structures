bc170b0e96947cdb13f100f61c121031dd8e54ea
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-01-24 16:12:02 +0200

    Add Heaps and Binary trees Lab solutions

--- Lesson_6_Heaps-BST-Lab-Skeleton/02.MaxHeap/MaxHeap.cs
+++ Lesson_6_Heaps-BST-Lab-Skeleton/02.MaxHeap/MaxHeap.cs
@@ -1,20 +1,76 @@
 ﻿namespace _02.MaxHeap
 {
     using System;
+    using System.Collections.Generic;
 
     public class MaxHeap<T> : IAbstractHeap<T>
         where T : IComparable<T>
     {
-        public int Size { get { throw new NotImplementedException(); } }
+        private List<T> elements;
+
+        public MaxHeap()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Size => this.elements.Count;
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this.elements.Add(element);
+
+            this.HeapifyUp();
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            return this.elements[0];
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException("MaxHeap is empty!");
+            }
+        }
+
+        private void HeapifyUp()
+        {
+            int currentIndex = this.Size - 1;
+            int parentIndex = this.GetParentIndex(currentIndex);
+
+            while (this.IndexIsValid(currentIndex) && this.IsGreater(currentIndex, parentIndex))
+            {
+                this.Swap(currentIndex, parentIndex);
+
+                currentIndex = parentIndex;
+                parentIndex = this.GetParentIndex(currentIndex);
+            }
+        }
+
+        private void Swap(int currentIndex, int parentIndex)
+        {
+            var temp = this.elements[currentIndex];
+            this.elements[currentIndex] = this.elements[parentIndex];
+            this.elements[parentIndex] = temp;
+        }
+
+        private int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        private bool IndexIsValid(int index)
+        {
+            return index > 0 && index < this.Size;
+        }
+
+        private bool IsGreater(int childIndex, int parentIndex)
+        {
+            return this.elements[childIndex].CompareTo(this.elements[parentIndex]) > 0;
         }
     }
 }
--- Lesson_6_Heaps-BST-Lab-Skeleton/03.PriorityQueue/PriorityQueue.cs
+++ Lesson_6_Heaps-BST-Lab-Skeleton/03.PriorityQueue/PriorityQueue.cs
@@ -1,25 +1,125 @@
 ﻿namespace _03.PriorityQueue
 {
     using System;
+    using System.Collections.Generic;
 
     public class PriorityQueue<T> : IAbstractHeap<T>
         where T : IComparable<T>
     {
-        public int Size { get { throw new NotImplementedException(); } }
+        private List<T> elements;
+
+        public PriorityQueue()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Size => this.elements.Count;
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            var firstElement = this.Peek();
+
+            this.Swap(0, this.Size - 1);
+
+            this.elements.RemoveAt(this.Size - 1);
+
+            this.HeapifyDown();
+
+            return firstElement;
         }
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this.elements.Add(element);
+
+            this.HeapifyUp();
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            return this.elements[0];
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException("MaxHeap is empty!");
+            }
+        }
+
+        private void HeapifyUp()
+        {
+            int currentIndex = this.Size - 1;
+            int parentIndex = this.GetParentIndex(currentIndex);
+
+            while (this.IndexIsValid(currentIndex) && this.IsGreater(currentIndex, parentIndex))
+            {
+                this.Swap(currentIndex, parentIndex);
+
+                currentIndex = parentIndex;
+                parentIndex = this.GetParentIndex(currentIndex);
+            }
+        }
+
+        private void HeapifyDown()
+        {
+            int index = 0;
+            int leftChildIndex = this.GetLeftChildIndex(index);
+
+            while (this.IndexIsValid(leftChildIndex) && this.IsLess(index, leftChildIndex))
+            {
+                int toSwapWith = leftChildIndex;
+                int rightChildIndex = this.GetRightChildIndex(index);
+
+                if (this.IndexIsValid(rightChildIndex) && this.IsLess(toSwapWith, rightChildIndex))
+                {
+                    toSwapWith = rightChildIndex;
+                }
+
+                this.Swap(toSwapWith, index);
+                index = toSwapWith;
+                leftChildIndex = this.GetLeftChildIndex(index);
+            }
+        }
+
+        private void Swap(int currentIndex, int parentIndex)
+        {
+            var temp = this.elements[currentIndex];
+            this.elements[currentIndex] = this.elements[parentIndex];
+            this.elements[parentIndex] = temp;
+        }
+
+        private int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        private int GetLeftChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 1;
+        }
+
+        private int GetRightChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 2;
+        }
+
+        private bool IndexIsValid(int index)
+        {
+            return index > 0 && index < this.Size;
+        }
+
+        private bool IsGreater(int childIndex, int parentIndex)
+        {
+            return this.elements[childIndex].CompareTo(this.elements[parentIndex]) > 0;
+        }
+
+        private bool IsLess(int childIndex, int parentIndex)
+        {
+            return this.elements[childIndex].CompareTo(this.elements[parentIndex]) < 0;
         }
     }
 }
