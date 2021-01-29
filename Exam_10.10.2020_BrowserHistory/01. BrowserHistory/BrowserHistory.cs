b97eda0a6ed1847a9e5e1e6a3382eb01c63999e0
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-01-29 08:10:13 +0200

    Add retake exam solution

---
+++ Exam_10.10.2020_BrowserHistory/01. BrowserHistory/BrowserHistory.cs
@@ -0,0 +1,215 @@
+﻿namespace _01._BrowserHistory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using _01._BrowserHistory.Interfaces;
+
+    public class BrowserHistory : IHistory
+    {
+        private Node<ILink> _head;
+        private Node<ILink> _tail;
+
+        public int Size { get; private set; }
+
+        public void Open(ILink link)
+        {
+            var toInsert = new Node<ILink>(link);
+
+            if (this.Size == 0)
+            {
+                this._head = this._tail = toInsert;
+            }
+            else
+            {
+                this._head.Previous = toInsert;
+                toInsert.Next = this._head;
+                this._head = toInsert;
+            }
+
+            this.Size++;
+        }
+
+        public void Clear()
+        {
+            this._head = this._tail = null;
+            this.Size = 0;
+        }
+
+        public bool Contains(ILink link)
+        {
+            return this.GetByUrl(link.Url) != null;
+        }
+
+        public ILink DeleteFirst()
+        {
+            this.EnsureNotEmpty();
+
+            var first = this._tail;
+
+            if (this.Size == 1)
+            {
+                this._head = this._tail = null;
+            }
+            else
+            {
+                this._tail = this._tail.Previous;
+                this._tail.Next = null;
+            }
+
+            this.Size--;
+            return first.Value;
+        }
+
+        public ILink DeleteLast()
+        {
+            this.EnsureNotEmpty();
+
+            var last = this._head;
+
+            if (this.Size == 1)
+            {
+                this._head = this._tail = null;
+            }
+            else
+            {
+                this._head = this._head.Next;
+                this._head.Previous = null;
+            }
+
+            this.Size--;
+            return last.Value;
+        }
+
+        public ILink GetByUrl(string url)
+        {
+            var current = this._head;
+
+            while (current != null)
+            {
+                if (current.Value.Url == url)
+                {
+                    return current.Value;
+                }
+
+                current = current.Next;
+            }
+
+            return null;
+        }
+
+        public ILink LastVisited()
+        {
+            this.EnsureNotEmpty();
+
+            return this._head.Value;
+        }
+
+
+        public int RemoveLinks(string url)
+        {
+            var current = this._head;
+            int count = 0;
+
+            while (current != null)
+            {
+                if (current.Value.Url.Contains(url))
+                {
+                    var previous = current.Previous;
+                    var next = current.Next;
+
+                    if (previous != null)
+                    {
+                        previous.Next = next;
+                    }
+                    else
+                    {
+                        this._head = this._head.Next;
+                    }
+
+                    if (next != null)
+                    {
+                        next.Previous = previous;
+                    }
+                    else
+                    {
+                        this._tail = this._tail.Previous;
+                    }
+
+                    count++;
+                    this.Size--;
+                }
+
+
+                current = current.Next;
+            }
+
+            if (count == 0)
+            {
+                throw new InvalidOperationException("No links with the given url!");
+            }
+
+            return count;
+        }
+
+        public ILink[] ToArray()
+        {
+            ILink[] result = new ILink[this.Size];
+            int index = 0;
+            var current = this._head;
+
+            while (current != null)
+            {
+                result[index++] = current.Value;
+
+                current = current.Next;
+            }
+
+            return result;
+        }
+
+        public List<ILink> ToList()
+        {
+            List<ILink> result = new List<ILink>(this.Size);
+            var current = this._head;
+
+            while (current != null)
+            {
+                result.Add(current.Value);
+
+                current = current.Next;
+            }
+
+            return result;
+        }
+
+        public string ViewHistory()
+        {
+            if (this.Size == 0)
+            {
+                return "Browser history is empty!";
+            }
+
+            StringBuilder output = new StringBuilder();
+            var current = this._head;
+
+            while (current != null)
+            {
+                output.AppendLine(current.Value.ToString());
+
+                current = current.Next;
+            }
+
+
+            return output.ToString();
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException("Browser history is empty!");
+            }
+        }
+    }
+}
---
+++ Exam_10.10.2020_BrowserHistory/02.DOM/DocumentObjectModel.cs
@@ -0,0 +1,245 @@
+﻿namespace _02.DOM
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using _02.DOM.Interfaces;
+    using _02.DOM.Models;
+
+    public class DocumentObjectModel : IDocument
+    {
+        public DocumentObjectModel(IHtmlElement root)
+        {
+            this.Root = root;
+        }
+
+        public DocumentObjectModel()
+        {
+            this.Root = new HtmlElement(
+                ElementType.Document,
+                new HtmlElement(ElementType.Html,
+                    new HtmlElement(ElementType.Head),
+                    new HtmlElement(ElementType.Body)
+                )
+            );
+        }
+
+        public IHtmlElement Root { get; private set; }
+
+        public IHtmlElement GetElementByType(ElementType type)
+        {
+            var queue = new Queue<IHtmlElement>();
+
+            queue.Enqueue(this.Root);
+
+            while (queue.Count > 0)
+            {
+                var currentEl = queue.Dequeue();
+
+                if (currentEl.Type == type)
+                {
+                    return currentEl;
+                }
+
+                foreach (var child in currentEl.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+
+        public List<IHtmlElement> GetElementsByType(ElementType type)
+        {
+            var result = new List<IHtmlElement>();
+
+            this.FindElementsByTypeDfs(this.Root, type, result);
+
+            return result;
+        }
+
+        public bool Contains(IHtmlElement htmlElement)
+        {
+            return this.FindHtmlElement(htmlElement) != null;
+        }
+
+        public void InsertFirst(IHtmlElement parent, IHtmlElement child)
+        {
+            this.CheckIfHtmlElementExists(parent);
+
+            parent.Children.Insert(0, child);
+            child.Parent = parent;
+        }
+
+        public void InsertLast(IHtmlElement parent, IHtmlElement child)
+        {
+            this.CheckIfHtmlElementExists(parent);
+
+            parent.Children.Add(child);
+            child.Parent = parent;
+        }
+
+        public void Remove(IHtmlElement htmlElement)
+        {
+            this.CheckIfHtmlElementExists(htmlElement);
+            this.RemoveReferences(htmlElement, htmlElement.Parent);
+        }
+
+        public void RemoveAll(ElementType elementType)
+        {
+            var queue = new Queue<IHtmlElement>();
+
+            queue.Enqueue(this.Root);
+
+            while (queue.Count > 0)
+            {
+                var currentEl = queue.Dequeue();
+
+                if (currentEl.Type == elementType)
+                {
+                    var parent = currentEl.Parent;
+
+                    this.RemoveReferences(currentEl, parent);
+                }
+                else
+                {
+                    foreach (var child in currentEl.Children)
+                    {
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+        }
+
+        public bool AddAttribute(string attrKey, string attrValue, IHtmlElement htmlElement)
+        {
+            this.CheckIfHtmlElementExists(htmlElement);
+
+            if (!htmlElement.Attributes.ContainsKey(attrKey))
+            {
+                htmlElement.Attributes.Add(attrKey, attrValue);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveAttribute(string attrKey, IHtmlElement htmlElement)
+        {
+            this.CheckIfHtmlElementExists(htmlElement);
+
+            if (htmlElement.Attributes.ContainsKey(attrKey))
+            {
+                htmlElement.Attributes.Remove(attrKey);
+                return true;
+            }
+
+            return false;
+        }
+
+        public IHtmlElement GetElementById(string idValue)
+        {
+            var queue = new Queue<IHtmlElement>();
+
+            queue.Enqueue(this.Root);
+
+            while (queue.Count > 0)
+            {
+                var currentEl = queue.Dequeue();
+                var attributes = currentEl.Attributes;
+
+                if (attributes.ContainsKey("id"))
+                {
+                    if (attributes["id"] == idValue)
+                    {
+                        return currentEl;
+                    }
+                }
+
+                foreach (var child in currentEl.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            this.CreateTextDfs(this.Root, result, 0);
+
+            return result.ToString();
+        }
+
+        private void CreateTextDfs(IHtmlElement current, StringBuilder result, int depth)
+        {
+            result
+                .AppendLine($"{new string(' ', depth)}{current.Type.ToString()}");
+
+            foreach (var child in current.Children)
+            {
+                this.CreateTextDfs(child, result, depth + 2);
+            }
+        }
+
+        private void FindElementsByTypeDfs(IHtmlElement current,
+            ElementType type,
+            List<IHtmlElement> result)
+        {
+            foreach (var child in current.Children)
+            {
+                this.FindElementsByTypeDfs(child, type, result);
+            }
+
+            if (current.Type == type)
+            {
+                result.Add(current);
+            }
+        }
+
+        private void CheckIfHtmlElementExists(IHtmlElement element)
+        {
+            var found = this.FindHtmlElement(element);
+
+            if (found == null)
+            {
+                throw new InvalidOperationException("Html element not found in DOM tree!");
+            }
+        }
+
+        private IHtmlElement FindHtmlElement(IHtmlElement htmlElement)
+        {
+            var queue = new Queue<IHtmlElement>();
+
+            queue.Enqueue(this.Root);
+
+            while (queue.Count > 0)
+            {
+                var currentEl = queue.Dequeue();
+
+                if (currentEl == htmlElement)
+                {
+                    return currentEl;
+                }
+
+                foreach (var child in currentEl.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+
+        private void RemoveReferences(IHtmlElement currentEl, IHtmlElement parent)
+        {
+            parent.Children.Remove(currentEl);
+            currentEl.Parent = null;
+            currentEl.Children.Clear();
+        }
+    }
+}
