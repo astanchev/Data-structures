fa65e626944bacee4b99ebd0fb538b29aae26ee8
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-02-11 20:16:43 +0200

    Implements Add to collection

--- Lesson_13_Hash-Tables-Sets-and-Dictionaries-Lab/HashTable/HashTable.cs
+++ Lesson_13_Hash-Tables-Sets-and-Dictionaries-Lab/HashTable/HashTable.cs
@@ -8,12 +8,9 @@
     {
         private LinkedList<KeyValue<TKey, TValue>>[] slots;
 
+        public const float LoadFactor = 0.75f;
         public const int InitialCapacity = 16;
         
-        public int Count { get; private set; }
-
-        public int Capacity => this.slots.Length;
-
         public HashTable()
         {
             this.slots = new LinkedList<KeyValue<TKey, TValue>>[InitialCapacity];
@@ -26,10 +23,33 @@
             this.Count = 0;
         }
 
+        public int Count { get; private set; }
+
+        public int Capacity => this.slots.Length;
+
         public void Add(TKey key, TValue value)
         {
-            throw new NotImplementedException();
-            // Note: throw an exception on duplicated key
+            GrowIfNeeded();
+
+            int slotNumber = this.FindSlotNumber(key);
+
+            if (this.slots[slotNumber] == null)
+            {
+                this.slots[slotNumber] = new LinkedList<KeyValue<TKey, TValue>>();
+            }
+
+            foreach (var element in this.slots[slotNumber])
+            {
+                if (element.Key.Equals(key))
+                {
+                    throw new ArgumentException("Key already exists: " + key);
+                }
+            }
+
+            var newElement = new KeyValue<TKey, TValue>(key, value);
+
+            this.slots[slotNumber].AddLast(newElement);
+            this.Count++;
         }
 
         public bool AddOrReplace(TKey key, TValue value)
@@ -105,6 +125,35 @@
         IEnumerator IEnumerable.GetEnumerator()
         {
             throw new NotImplementedException();
+        }
+
+        private void GrowIfNeeded()
+        {
+            if ((float)(this.Count + 1) / this.Capacity > LoadFactor)
+            {
+                // Hash table loaded too much --> resize
+                this.Grow();
+            }
+        }
+
+        private void Grow()
+        {
+            var newHashTable = new HashTable<TKey, TValue>(2 * this.Capacity);
+
+            foreach (var element in this)
+            {
+                newHashTable.Add(element.Key, element.Value);
+            }
+
+            this.slots = newHashTable.slots;
+            this.Count = newHashTable.Count;
+        }
+
+        private int FindSlotNumber(TKey key)
+        {
+            var slotNumber = Math.Abs(key.GetHashCode()) % this.slots.Length;
+
+            return slotNumber;
         }
     }
 }
