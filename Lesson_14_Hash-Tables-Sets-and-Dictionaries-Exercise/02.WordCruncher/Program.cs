40a4e38b74ab018a560a5a25c5e78b4ecc9e58d6
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-02-17 21:08:11 +0200

    Add WordCruncher

---
+++ Lesson_14_Hash-Tables-Sets-and-Dictionaries-Exercise/02.WordCruncher/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.WordCruncher
+{
+    using System;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(", ");
+            var target = Console.ReadLine();
+
+            WordCruncher wc = new WordCruncher(input, target);
+
+            foreach (var path in wc.GetPaths())
+            {
+                Console.WriteLine(string.Join(" ", path));
+            }
+        }
+    }
+}
